10fad9099d768542846cf2515cd0ea22773bb07b
Timur Kartaev <contr.timur.kartaev@pandadoc.com>
2025-05-15 20:39:57 +0500

    configure for vercel deloyment

--- chimera/settings.py
+++ chimera/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'chimera.wsgi.application'
+WSGI_APPLICATION = 'chimera.wsgi.app'
 
 
 # Database
