b85a195c9ef6286dfcba6b3a7b36c1558996f0de
Doniyor Rufatov <contr.doniyor.rufatov@pandadoc.com>
2025-06-11 00:04:57 +0500

    add custom middleware for XFrameOptions

---
+++ chimera/gong_middleware.py
@@ -0,0 +1,9 @@
+class AllowFromGongMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        # Replace with Gong's actual domain if known
+        response["X-Frame-Options"] = "ALLOWALL"
+        return response
--- chimera/settings.py
+++ chimera/settings.py
@@ -90,6 +90,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "chimera.gong_middleware.AllowFromGongMiddleware",
 ]
 
 ROOT_URLCONF = "chimera.urls"
